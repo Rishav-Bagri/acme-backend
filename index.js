a9b1e02baba1dc119125caf14e4210c4944129fa
Rishav <bagririshav01@gmail.com>
2025-04-12 19:47:10 +0530

    someedit

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use((err, req, res, next) => {
 })
 
 // dynamic port (important for Railway)
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 8080
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
